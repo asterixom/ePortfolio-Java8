e7f9105ade2aebe7cbf9683182c3f095a4ba83dc
Oliver <oliver@fam-mahlke.de>
2015-06-08 12:45:06 +0200

    Added other possible implementations

--- ePortfolio/src/de/oliver/ePortfolio/java8/Lambdas/TestKlasse.java
+++ ePortfolio/src/de/oliver/ePortfolio/java8/Lambdas/TestKlasse.java
@@ -4,8 +4,29 @@ import java.util.function.Consumer;
 
 public class TestKlasse {
 	public static void main(String[] args) {
-		double erg = calculate(2.0,10.0,(a,b)->a+b);
-		consume(erg,System.out::println);
+		double erg1 = calculate(2.0, 10.0, new FunktionalInterface() {
+			
+			@Override
+			public double calc(double a, double b) {
+				return a+b;
+			}
+		});
+		double erg2 = calculate(2.0,10.0,(c,b)->{
+			return c+b;
+			});
+		
+		double erg3 = calculate(2.0, 10.0, (a,b)->a+b);
+		
+		
+		consume(erg1,new Consumer<Double>() {
+			
+			@Override
+			public void accept(Double t) {
+				System.out.println(t);
+			}
+		});
+		consume(erg2,(x)->System.out.println(x));
+		consume(erg3,System.out::println);
 	}
 	
 	public static double calculate(double a, double b, FunktionalInterface c){
